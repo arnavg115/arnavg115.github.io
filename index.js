1b920a80a1124ead5d69e2d546d0055d833b8f81
Arnav G <arnavg286@gamil.com>
2021-09-18 23:26:45 -0700

    changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typer", {
-  strings: ["", "programmer", "student"],
+  strings: ["", "programmer.", "student.", "photographer."],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 3000,
