7e66ed08d7d4222b69e56869c9326960f20c6f26
Arnav G <arnavg286@gamil.com>
2021-09-23 20:38:28 -0700

    changes

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ var typed = new Typed(".typer", {
   loop: true,
   loopCount: Infinity,
 });
+
+function onLoad() {
+  var val = "true" === localStorage.getItem("dark");
+  if (val) {
+    document.getElementById("checker").checked = true;
+    body.classList.remove("light");
+    body.classList.add("dark");
+  } else {
+    document.getElementById("checker").checked = false;
+    body.classList.remove("dark");
+    body.classList.add("light");
+  }
+}
+
+function changeTheme() {
+  var t = document.getElementById("checker");
+  var body = document.getElementById("body");
+
+  if (t.checked) {
+    body.classList.remove("light");
+    body.classList.add("dark");
+  } else {
+    body.classList.remove("dark");
+    body.classList.add("light");
+  }
+  localStorage.setItem("dark", t.checked.toString());
+}
